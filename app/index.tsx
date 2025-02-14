a2e3abdb4be4f3986b9164744f86651fe08427ea
Yatik1 <yatiksrivastava1@gmail.com>
2025-02-14 19:30:05 +0530

    Create initial landing page with login, signup, and guest options

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,63 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+        <Image source={require('@/assets/chazel_hero.jpg')} style={{flex:1,objectFit:"contain",width:700}} />
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity style={[styles.button, {backgroundColor:"black", borderColor:"gray",borderWidth:1}]}>
+                <Text style={[styles.buttonText,{color:"white"}]}>Login with email</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, {backgroundColor:"white"}]}>
+                <Text style={[styles.buttonText]}>Sign up your account </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button,{borderColor:"black",borderWidth:1}]}>
+                <Text style={[styles.buttonText,{color:"drakgray"}]}>Use guest account</Text>
+            </TouchableOpacity>
+        </View>
     </View>
   )
 }
 
+
+
 export default index
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:"#C7ADED"
+    },
+    buttonContainer:{
+        gap:20,
+        flex:1,
+        marginTop:36
+    },
+    button:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:"center",
+        paddingVertical:13,
+        paddingHorizontal:40,
+        borderRadius:10,
+        gap:10,
+      },
+      buttonText:{
+        fontSize:20,
+        fontWeight:'500'
+      },
+      description:{
+        fontSize:12,
+        textAlign:'center',
+        // color:Colors.lightText
+      },
+      link:{
+        // color:Colors.lightText,
+        fontSize:12,
+        textAlign:'center',
+        textDecorationLine:'underline'
+      }
+})

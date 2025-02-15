bb37001e4e3bdf5cbf95a51849dbab6d2e02ece4
Yatik1 <yatiksrivastava1@gmail.com>
2025-02-15 17:22:49 +0530

    Enhance login screen with email and password input fields

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,12 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
 const index = () => {
+
+  const [email,setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Login to Chazel 🫡</Text>
+      <View>
+          <TextInput 
+            style={styles.field}
+            autoCapitalize='none'
+            placeholder='Email'
+            keyboardType='email-address'
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput 
+            style={styles.field}
+            autoCapitalize='none'
+            placeholder='Password'
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+          />
+          
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Text>{showPassword ? "Hide" : "Show"} Password</Text>
+          </TouchableOpacity>
+
+      </View>
     </View>
   )
 }
 
 export default index
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  header:{
+    fontWeight:'800',
+    fontSize:25,
+    color:'purple'
+  },
+  field:{
+      paddingVertical:15,
+      paddingHorizontal:20,
+      borderWidth:0.5,
+      borderRadius:10,
+      width:300,
+      fontSize:17,
+      borderColor:"gray"
+  }
+})

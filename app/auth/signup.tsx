cb5ad8af1de12514a5ab7300375c75aa331f973a
Yatik1 <yatiksrivastava1@gmail.com>
2025-02-15 18:14:01 +0530

    Implement signup screen with form inputs and styling

--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -1,12 +1,127 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Touchable } from 'react-native'
+import React, { useState } from 'react'
 
-const signup = () => {
+const index = () => {
+
+  const [name, setName] = useState<string>("")
+  const [email,setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+  const [confirmPassword, setConfirmPassword] = useState<string>("")
+  // const [profile, setProfile] = useState<string|null>("")
+
+  function handleLogin() {
+    let data = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    }
+    console.log(data)
+  }
+
   return (
-    <View>
-      <Text>signup</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Create account 👤</Text>
+      <View style={styles.signedBox}>
+          <TextInput 
+            style={styles.field}
+            autoCapitalize='words'
+            placeholder='Name'
+            keyboardType='default'
+            value={name}
+            onChangeText={setName}
+          />
+
+          <TextInput 
+            style={styles.field}
+            autoCapitalize='none'
+            placeholder='Email'
+            keyboardType='email-address'
+            value={email}
+            onChangeText={setEmail}
+          />
+          
+          <View style={{display:"flex",gap:8}}>
+            <TextInput 
+              style={styles.field}
+              autoCapitalize='none'
+              placeholder='Password'
+              secureTextEntry={!showPassword}
+              value={password}
+              onChangeText={setPassword}
+            />
+
+            <TouchableOpacity style={{width:120, backgroundColor:"lightgray", borderWidth:0.5, padding:5, borderRadius:4, display:"flex",alignItems:"center",justifyContent:"center"}} onPress={() => setShowPassword(!showPassword)}>
+              <Text>{showPassword ? "Hide" : "Show"} Password</Text>
+            </TouchableOpacity>
+          </View> 
+
+          <View style={{display:"flex",gap:8}}>
+            <TextInput 
+              style={styles.field}
+              autoCapitalize='none'
+              placeholder='Password'
+              secureTextEntry={!showPassword}
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+            />
+
+            <TouchableOpacity style={{width:120, backgroundColor:"lightgray", borderWidth:0.5, padding:5, borderRadius:4, display:"flex",alignItems:"center",justifyContent:"center"}} onPress={() => setShowPassword(!showPassword)}>
+              <Text>{showPassword ? "Hide" : "Show"} Password</Text>
+            </TouchableOpacity>
+          </View>
+
+
+
+          <TouchableOpacity style={[styles.button, {backgroundColor:"black",marginTop:30}]} onPress={handleLogin}>
+            <Text style={[styles.buttonText, {color:"white"}]}>Signup</Text>
+          </TouchableOpacity>
+
+      </View>
     </View>
   )
 }
 
-export default signup
+export default index
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+    gap:35,
+    paddingVertical:100,
+    backgroundColor:"white"
+  },
+  header:{
+    fontWeight:'800',
+    fontSize:30,
+    color:'purple'
+  },
+  signedBox:{
+    gap:10
+  },
+  field:{
+      paddingVertical:15,
+      paddingHorizontal:17,
+      borderWidth:0.5,
+      borderRadius:10,
+      width:300,
+      fontSize:17,
+      borderColor:"gray"
+  },
+  button:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:"center",
+    paddingVertical:13,
+    paddingHorizontal:40,
+    borderRadius:10,
+    gap:10,
+  },
+  buttonText:{
+    fontSize:20,
+    fontWeight:'500'
+  }
+})

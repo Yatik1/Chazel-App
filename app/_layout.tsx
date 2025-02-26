01e7a755b75961db79084e73198ab61920537677
Yatik1 <yatiksrivastava1@gmail.com>
2025-02-26 15:05:53 +0530

    Remove theme management: eliminate ThemeProvider and related color scheme logic from RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,12 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 
 export default function RootLayout() {
 
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{headerShown:false}}>
         <Stack.Screen name='index' />
 
@@ -31,6 +28,5 @@ export default function RootLayout() {
           name='(authenticated)'
         />
       </Stack>
-    </ThemeProvider>
   );
 }

a6176af2d04118f1641655261e5c580fdf21f15d
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-04 23:33:02 +0530

    Refactor layout component to include a search field and enhance header styles for authenticated tabs

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -2,11 +2,11 @@ import { Tabs } from "expo-router";
 import Entypo from '@expo/vector-icons/Entypo';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, TextInput, View } from "react-native";
+
 
 export default function Layout() {
-
-    const {bottom} = useSafeAreaInsets()
 
     return(
         <Tabs
@@ -14,12 +14,12 @@ export default function Layout() {
                 tabBarActiveTintColor:"black",
                 tabBarInactiveTintColor:"gray",
                 tabBarLabelStyle:{
-                    fontSize:12,
+                    fontSize:13,
                 },
                 tabBarStyle:{
                     height:68,
                     paddingTop:6,
-                }
+                },
             }}
         >
             <Tabs.Screen 
@@ -27,22 +27,43 @@ export default function Layout() {
                 options={{
                     title:"Chats",
                     tabBarIcon:({color}) => <Entypo name="chat" size={24} color={color} />,
+                    headerStyle:{
+                        height:105,
+                    },
+                    headerTitleStyle:{
+                        fontSize:40,
+                        fontWeight:"bold",
+                        color:"#1E1E1E",
+                    },
+                    headerTitleAlign:"left"
                 }}
             />
             <Tabs.Screen 
                 name="browse/index" 
                 options={{
                     title:"Browse",
-                    tabBarIcon:({color}) => <MaterialCommunityIcons name="web" size={24} color={color} />
+                    headerTitle:() => <SearchField />,
+                    tabBarIcon:({color}) => <MaterialCommunityIcons name="web" size={24} color={color} />,
                 }}
             />
             <Tabs.Screen 
                 name="profile/index"
                 options={{
                     title:"Profile",
-                    tabBarIcon:({color}) => <AntDesign name="user" size={24} color={color} />
+                    tabBarIcon:({color}) => <AntDesign name="user" size={24} color={color} />,
+                    headerShown:false,
                 }}
             />
         </Tabs>
+    )
+}
+
+function SearchField(){
+    return(
+            <View style={{flexDirection:"row", alignItems:"center"}}>
+                <AntDesign name="search1" size={24} color="black" />
+                <TextInput />
+            </View>
+
     )
 }

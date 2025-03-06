67441f577ba442a606eb558b01f336bb54305f2f
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-06 12:56:35 +0530

    Display Chats component based on user state in chats tab

--- app/(authenticated)/(tabs)/chats/index.tsx
+++ app/(authenticated)/(tabs)/chats/index.tsx
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { ChatState } from '@/context/ChatProvider'
+import UserType from '@/types/user'
+import Chats from '@/component/Chats'
 
 const index = () => {
+
+  const [fetchAgain,setFetchAgain] = React.useState<boolean>(false)
+  const {user} = ChatState() as {user:UserType}
+
   return (
-    <View>
-      <Text>Chats</Text>
+    <View style={{flex:1}}>
+      {user && <Chats fetchAgain={fetchAgain} />}
     </View>
   )
 }

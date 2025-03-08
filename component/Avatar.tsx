398f7d699bdfd142a6568af59ef8d2932c25845c
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-08 12:49:12 +0530

    Add Avatar component for user profile display with initials fallback

---
+++ component/Avatar.tsx
@@ -0,0 +1,65 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React from 'react'
+
+interface ChatUserType {
+    _id:string,
+    name:string, 
+    email:string,
+    pic:string | null,
+    isAdmin:boolean
+}
+
+type SenderProps = {
+    chatName:string,
+    groupAdmin:ChatUserType,
+    isGroupChat:boolean,
+    users:ChatUserType[],
+    _id:string
+
+}
+
+
+function Avatar({sender}: {sender: SenderProps | ChatUserType}) {
+
+    function getInitials(sender:any) {
+        if(sender.isGroupChat) {
+            return sender.chatName[0] 
+        } else {
+            return sender.name[0]
+        }
+    }
+
+    const creds = getInitials(sender)
+    
+    return (
+            'pic' in sender && sender.pic ? 
+                <Image source={{uri:sender.pic}} style={styles.avatar} /> 
+                : 
+                <View style={styles.avatar}>
+                    <Text style={styles.avatarText}>{creds}</Text>
+                </View>
+    )
+}
+
+export default Avatar
+
+const styles = StyleSheet.create({
+    avatar: {
+        width: 35, 
+        height: 35, 
+        borderRadius:100,
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        objectFit:"cover",
+        overflow:"hidden",
+        backgroundColor:"#555555"
+    },
+    avatarText:{
+        color:"white", 
+        fontSize:20, 
+        fontWeight:"bold", 
+        textTransform:'uppercase',
+        textAlign:"center"
+    }
+})

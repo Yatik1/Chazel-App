914a0f5e255035e27204986ee88eade31e2f1885
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-08 12:51:17 +0530

    Add ChatUserType interface and SenderProps type for improved chat functionality

--- types/user.ts
+++ types/user.ts
@@ -5,3 +5,20 @@ export default interface UserType {
     token:string,
     _id:string
 }
+
+export interface ChatUserType {
+    _id:string,
+    name:string, 
+    email:string,
+    pic:string | null,
+    isAdmin:boolean
+}
+
+export type SenderProps = {
+    chatName:string,
+    groupAdmin:ChatUserType,
+    isGroupChat:boolean,
+    users:ChatUserType[],
+    _id:string
+
+}

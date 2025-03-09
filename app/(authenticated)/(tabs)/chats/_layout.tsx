36443fc8e531bb27ab42937249fd460b0970cefb
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-09 15:15:04 +0530

    Add layout component for chat screens with custom header

---
+++ app/(authenticated)/(tabs)/chats/_layout.tsx
@@ -0,0 +1,33 @@
+import { ChatState } from "@/context/ChatProvider";
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Layout() {
+
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{
+                headerTitle:"Chats",
+                header : () =>  <Header />
+            }}/>
+            <Stack.Screen 
+                name="[chatId]"
+                options={{
+                    headerTitle:"Chat",
+                    headerBackButtonDisplayMode:"minimal",
+                    headerTintColor:"black"
+                }}
+            />
+        </Stack>
+    )
+}
+
+function Header() {
+    const {top} = useSafeAreaInsets()
+    return (
+        <View style={{backgroundColor:"white", display:"flex",justifyContent:"flex-end",alignItems:"flex-start",paddingTop:top+20}}>
+            <Text style={{fontSize:40,fontWeight:"bold",padding:8}}>Chats</Text>
+        </View>
+    )
+}

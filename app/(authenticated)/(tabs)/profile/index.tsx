b339998b4a7fd324ba3b39d68511e4ccdcd97d98
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-09 15:15:21 +0530

    Refactor profile component to include Avatar and update user name styling

--- app/(authenticated)/(tabs)/profile/index.tsx
+++ app/(authenticated)/(tabs)/profile/index.tsx
@@ -4,6 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useRouter } from 'expo-router'
 import { ChatState } from '@/context/ChatProvider'
 import UserType from '@/types/user'
+import Avatar from '@/component/Avatar'
 
 const index = () => {
 
@@ -15,18 +16,16 @@ const index = () => {
     await AsyncStorage.removeItem("userInfo")
     route.replace("/")
   }
-  
 
   return (
     <View style={styles.container}>
       <View style={{alignItems:'center',marginBottom:20}}>
-        <Text style={{fontSize:30, fontWeight:"bold",marginBottom:25}}>Profile</Text>
         <View style={{display:"flex",backgroundColor:"#C7C7C7",flexDirection:"row-reverse",padding:10,gap:10,alignItems:"center",justifyContent:"center",borderRadius:10,borderWidth:0.5}}>
           <View style={{display:"flex",flexDirection:"column", justifyContent:"center",alignItems:"center",gap:2}}>
-            <Text style={{fontSize:18,fontWeight:"500"}}>{user.name}</Text>
+            <Text style={{fontSize:20,fontWeight:"500"}}>{user.name}</Text>
             <Text style={{fontSize:11}}>{user.email}</Text>
           </View>
-          {user.pic && <Image source={{uri:user.pic}} style={{width:55,height:55,borderRadius:100}} />}
+          <Avatar sender={user} />
         </View>
       </View>
       <TouchableOpacity style={styles.button} onPress={logout}>
@@ -43,7 +42,6 @@ const styles = StyleSheet.create({
     flex:1,
     alignItems:'center',
     justifyContent:'center',
-    backgroundColor:'white'
   },
   button:{
     flexDirection:'row',

bdac5306dd57d47d601fae16d50f28e5bed91e41
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-12 14:10:21 +0530

    Remove unused import of ChatHeader in chat page component

--- app/(authenticated)/(tabs)/chats/[chatId].tsx
+++ app/(authenticated)/(tabs)/chats/[chatId].tsx
@@ -1,7 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
-import ChatHeader from '@/component/ChatHeader'
 
 const Page = () => {
   

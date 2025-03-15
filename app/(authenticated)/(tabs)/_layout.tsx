0fe7effb74614949e7c25046e44921e78c2a7ccb
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-15 13:11:39 +0530

    Add path validation to hide tab bar on specific routes

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -1,14 +1,21 @@
-import { Tabs } from "expo-router";
+import { Tabs, usePathname } from "expo-router";
 import Entypo from '@expo/vector-icons/Entypo';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { TextInput, View } from "react-native";
-import { useEffect, useState } from "react";
-import { ChatState } from "@/context/ChatProvider";
-import axios from "axios";
+import BrowseField from "@/component/BrowseField";
+import { useNavigationState } from "@react-navigation/native";
+
 
 
 export default function Layout() {
+
+const path = usePathname()
+
+let isValidPath = true
+if(path !== "/chats" && path !== "/browse" && path !== "/profile") {
+    isValidPath = !isValidPath
+}
+
 
     return(
         <Tabs
@@ -18,9 +25,11 @@ export default function Layout() {
                 tabBarLabelStyle:{
                     fontSize:13,
                 },
-                tabBarStyle:{
+                tabBarStyle: isValidPath ? {
                     height:68,
                     paddingTop:6,
+                } : {
+                    display:'none'
                 },
             }}
         >
@@ -50,60 +59,5 @@ export default function Layout() {
                 }}
             />
         </Tabs>
-    )
-}
-
-function BrowseField(){
-    const [search,setSearch] = useState("")
-    const {user,searchResult,setSearchResult} = ChatState() as any
-
-
-    const searchedUser = async () => {
-        if(!search || search === " ") {
-            console.log("Nothing to be search for...")
-            setSearchResult([])
-            return;
-        }
-
-        try {
-            
-            const config = {
-                headers : {
-                    Authorization : `Bearer ${user.token}`
-                }
-            }
-
-            const {data} = await axios.get(`https://chat-app-9flg.onrender.com/api/user?search=${search}` , config)
-            setSearchResult(data)
-        } catch (error) {
-            console.log("Error in seaching", error)
-        }
-    }
-    
-    useEffect(() => {
-        searchedUser()
-    } , [search]) 
-
-    return(
-            <View style={{flexDirection:"row", alignItems:"center", justifyContent:"space-around", gap:10,padding:3}}>
-                <AntDesign name="search1" size={20} color="#1E1E1E" />
-                <TextInput 
-                    style={{
-                        paddingVertical:5,
-                        paddingHorizontal:10,
-                        borderWidth:0.5,
-                        borderRadius:5,
-                        width:300,
-                        fontSize:14,
-                        borderColor:"lightgray"
-                    }}
-                    autoCapitalize='words'
-                    placeholder='Browse...'
-                    keyboardType='default'
-                    value={search}
-                    onChangeText={setSearch}
-                />
-            </View>
-
     )
 }

2d455f943002b9158d81f2910878ed5cda86d7b0
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-15 14:14:36 +0530

    Add message input field with send button to chat page

--- app/(authenticated)/(tabs)/chats/[chatId].tsx
+++ app/(authenticated)/(tabs)/chats/[chatId].tsx
@@ -1,16 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Page = () => {
   
   const {chatId} = useLocalSearchParams<{chatId:string}>()
   return (
-    <View>
+    <View style={{flex:1, alignItems:'center', justifyContent:'space-between', backgroundColor:"white"}}>
       <Text>chatId: {chatId}</Text>
+      <Field />
     </View>
   )
 }
 
 export default Page
 
+function Field() {
+
+  const {bottom} = useSafeAreaInsets()
+  
+  const [message, setMessage] = useState<string>("")
+  const isMessage = message.length > 0;
+
+  return (
+    <View style={[styles.wrapper, {paddingBottom:bottom-8}]}>
+      <TextInput 
+        style={styles.field}
+        placeholder='Message....'
+        keyboardType='default'
+        value={message}
+        onChangeText={setMessage}
+        autoCapitalize='none'
+        autoCorrect={false}
+        autoFocus={false}
+      />
+      {isMessage && (
+        <TouchableOpacity style={styles.outerCircle} onPress={() => console.log(message)}>
+          <Ionicons name="send" size={24} color="white" />
+        </TouchableOpacity>
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper:{
+    display:"flex",
+    flexDirection:"row",
+    gap:8,
+    alignItems:"center",
+    justifyContent:"center",
+    paddingTop:10,
+    width:"100%",
+    paddingHorizontal:10,
+  },
+  field:{
+    padding:10,
+    borderWidth:0.3,
+    borderRadius:100,
+    fontSize:15,
+    borderColor:"gray",
+    flex:1,
+},
+outerCircle:{
+  backgroundColor:"black",
+  display:"flex",
+  justifyContent:"center",
+  alignItems:"center",
+  padding:"auto",
+  width:40,
+  height:40,
+  borderRadius:"100%"
+}
+})

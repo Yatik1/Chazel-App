db197f35f3064f09bc0645736c334424341627b8
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-17 15:42:00 +0530

    Fetch and display messages for the selected chat in ChatBox component

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -1,15 +1,45 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ChatState } from '@/context/ChatProvider'
+import axios from 'axios'
+import AllChats from './AllChats'
 
-const ChatBox = () => {
+const ChatBox = ({messages,setMessages} : {messages:string[],setMessages:React.Dispatch<React.SetStateAction<string[]>>}) => {
 
   const {selectedChat, user} = ChatState() as any
-  console.log(selectedChat)
+  // console.log(selectedChat)
+
+  const fetchChats = async () => {
+    if(!selectedChat) return;
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`
+        }
+      }
+
+      const {data} = await axios.get(
+        `https://chat-app-9flg.onrender.com/api/message/${selectedChat._id}`,
+        config
+      )
+
+      setMessages(data)
+      
+    } catch (error) {
+      console.log("Error fetching chats", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchChats()
+  },[selectedChat])
 
   return (
-    <View style={{flex:1,justifyContent:'center',alignItems:"center"}}>
-      <Text>Start your conversation</Text>
+    <View style={{flex:1,width:"100%", height:"100%"}}>
+        <View style={{flex:1,flexDirection:"column", justifyContent:"flex-end", alignItems:"flex-end",padding:10}}>
+          <AllChats messages={messages} />
+        </View>
     </View>
   )
 }

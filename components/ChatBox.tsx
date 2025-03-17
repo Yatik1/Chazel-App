3ec8fef2bdf2764469d7dbaf96a4fefbf68ea2fb
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-17 12:42:01 +0530

    Add ChatState context to ChatBox for managing selected chat and user state

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -1,7 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { ChatState } from '@/context/ChatProvider'
 
 const ChatBox = () => {
+
+  const {selectedChat, user} = ChatState() as any
+  console.log(selectedChat)
+
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:"center"}}>
       <Text>Start your conversation</Text>

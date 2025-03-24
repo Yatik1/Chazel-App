a67967de757166685b89bc8975ec8d3355506750
Yatik1 <yatiksrivastava1@gmail.com>
2025-03-24 12:41:52 +0530

    Refactor ChatBox layout for improved structure and alignment

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -37,7 +37,7 @@ const ChatBox = ({messages,setMessages} : {messages:string[],setMessages:React.D
 
   return (
     <View style={{flex:1,width:"100%", height:"100%"}}>
-        <View style={{flex:1,flexDirection:"column", justifyContent:"flex-end", alignItems:"flex-end",padding:10}}>
+        <View style={{flex:1,display:"flex",flexDirection:"column",padding:10}}>
           <AllChats messages={messages} />
         </View>
     </View>
